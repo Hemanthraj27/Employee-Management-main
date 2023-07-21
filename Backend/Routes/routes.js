28b59bf463d7351c7c78fefbab289c4db5ead247
Hemanthraj27 <hemanthrajsaravanan@gmail.com>
2023-07-21 15:00:19 +0530

    Hello First Commit

---
+++ Backend/Routes/routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import {
+   updateEmployeeById, 
+   deleteEmployeeById,
+} from '../Controller/employeeConroller.js'
+
+const router = express.Router();
+
+// Update user
+router.put('/employees/:emp_id', updateEmployeeById);
+
+// Delete user
+router.delete('/employees/:emp_id', deleteEmployeeById);
+
+export default router;
+
