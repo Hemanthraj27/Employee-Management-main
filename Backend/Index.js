171803a68682392858836b552cc12ead23bdcd82
Hemanthraj27 <hemanthrajsaravanan@gmail.com>
2023-07-25 12:10:24 +0530

    commit

--- Backend/Index.js
+++ Backend/Index.js
@@ -4,8 +4,12 @@ import bodyParser from "body-parser";
 // import routes
 import router from './Routes/routes.js';
 
-const app = express();
+import dotenv from 'dotenv';
+import { connectToDatabase } from './Services/mongoDBService.js';
 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
 
 // use express json
 app.use(bodyParser.json());
@@ -16,13 +20,13 @@ app.use(router);
 
 // Middleware to parse request body as JSON
 app.use(express.json());
-
 app.get('/', function(req, res){
   res.json({ message: 'Welcome to Securra Health' });
 });
 
-// Start server
-const port = 4000; 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Connect to MongoDB and start the server
+connectToDatabase().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
